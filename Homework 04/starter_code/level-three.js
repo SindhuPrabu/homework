3469a1dce7f44249e85b01d7c252d017989b5d98
SindhuPrabu <sindhu.prabu@gmail.com>
2022-10-16 21:41:14 -0400

    Homework 04

--- Homework 04/starter_code/level-three.js
+++ Homework 04/starter_code/level-three.js
@@ -5,3 +5,22 @@
 let score = 0;
 let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
+
+//target the classlist property when the js-ball is clicked 
+//defening dataset data and its property is called increment 
+//parseInt is converting the string data-increment="20" to integer
+
+arenaEl.addEventListener('click', function(play_game) {
+    if ( play_game.target.classList.contains('js-ball') ) {
+      score += parseInt(play_game.target.dataset.increment);
+      if ( score < 100 ) {
+        scoreEl.innerText = score;
+      } else {
+        declareWinner();
+      }
+    }
+  })
+  
+  function declareWinner() {
+    document.body.classList.add('game-over');
+  }
--- Homework 04/starter_code/level-two.js
+++ Homework 04/starter_code/level-two.js
@@ -6,4 +6,22 @@ let score = 0;
 let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
+//target the classlist property when the js-ball is clicked
+
+arenaEl.addEventListener('click', function(play_game) {
+    if (play_game.target.classList.contains('js-ball') ) {
+      score += 10;
+      if ( score < 100 ) {
+        scoreEl.innerText = score;
+      } else {
+        declareWinner();
+      }
+    }
+  })
+  
+  function declareWinner() {
+    document.body.classList.add('game-over');
+  }
+  
+
 
